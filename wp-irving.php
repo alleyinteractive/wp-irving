244cd46fb27c2fc417ba100c62cca861fa9e6762
James Burke <james.walter.burke@gmail.com>
2018-03-21 22:23:17 -0400

    Inital commit

---
+++ wp-irving.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Entry point for the plugin.
+ *
+ * Plugin Name: WP Irving
+ * Description: The WordPress side of Irving - Alley's React Ecosystem
+ * Author: Alley Interactive, jameswburke
+ * Version: 0.1
+ * Author URI: http://alleyinteractive.com
+ *
+ * @package Irving
+ */
+
+define( 'IRVING_PATH', dirname( __FILE__ ) );
