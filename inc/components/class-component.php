a7ec189b3a0d893c3b81c6be2b8c96bbc8b3610f
James Burke <james@alley.co>
2018-06-08 21:42:40 -0400

    Docblocks

--- inc/components/class-component.php
+++ inc/components/class-component.php
@@ -1,16 +1,46 @@
 <?php
 /**
+ * Parent class file for Irving's Components.
  *
+ * @package WP_Irving
  */
 
 namespace WP_Irving\Component;
 
+/**
+ * Defines the general component class.
+ */
 class Component {
 
+	/**
+	 * Unique component slug.
+	 *
+	 * @var string
+	 */
 	public $name = '';
+
+	/**
+	 * Component config.
+	 *
+	 * @var array
+	 */
 	public $config = [];
+
+	/**
+	 * Component children.
+	 *
+	 * @var array
+	 */
 	public $children = [];
 
+	/**
+	 * Component constructor.
+	 *
+	 * @param string $name     Unique component slug or array of name, config,
+	 *                         and children value.
+	 * @param array  $config   Component config.
+	 * @param array  $children Component children.
+	 */
 	public function __construct( $name = '', array $config = [], array $children = [] ) {
 
 		// Allow $name to be passed as a config array.
@@ -29,6 +59,11 @@ class Component {
 		return $this;
 	}
 
+	/**
+	 * Output properties as an array to be used as JSON.
+	 *
+	 * @return array Component properties.
+	 */
 	public function json() {
 		return [
 			'name'     => $this->name,
@@ -38,6 +73,14 @@ class Component {
 	}
 }
 
+/**
+ * Helper to generate a generic component.
+ *
+ * @param  string $name     Component name or array of properties.
+ * @param  array  $config   Component config.
+ * @param  array  $children Component children.
+ * @return Component An instance of the Component class.
+ */
 function component( $name = '', array $config = [], array $children = [] ) {
 	return new Component( $name, $config, $children );
 }
