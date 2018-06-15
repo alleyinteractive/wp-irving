bf20392df0d04fb818a106dee003eb4706843676
James Burke <james@alley.co>
2018-06-15 09:11:24 -0600

    Menu component.

--- inc/components/class-menu.php
+++ inc/components/class-menu.php
@@ -37,6 +37,18 @@ class Menu extends Component {
 	 * @return Menu An instance of the Menu class.
 	 */
 	public function parse_wp_menu_by_location( string $menu_location ) {
+
+		// Get menu locations.
+		$locations = get_nav_menu_locations();
+
+		// Get object id by location.
+		$menu_term = wp_get_nav_menu_object( $locations[ $menu_location ] ?? null );
+
+		// If valid menu term.
+		if ( $menu_term instanceof \WP_Term ) {
+			$this->parse_wp_menu( $menu_term->slug );
+		}
+
 		return $this;
 	}
 
@@ -49,7 +61,54 @@ class Menu extends Component {
 	public function parse_wp_menu( string $menu_slug ) {
 		$menu_items = wp_get_nav_menu_items( $menu_slug );
 
+		$this->children = $this->build_menu( $menu_items );
+
 		return $this;
+	}
+
+	/**
+	 * Recursive function to build a complete menu with children menu items.
+	 *
+	 * @param  array   $menu_items Menu items.
+	 * @param  integer $parent_id  Parent menu ID.
+	 * @return array All menu items.
+	 */
+	public function build_menu( $menu_items, $parent_id = 0 ) {
+
+		// Start building menu.
+		$menu = [];
+
+		// Loop through all menu items.
+		foreach ( (array) $menu_items as $key => $menu_item ) {
+
+			// Current menu's id.
+			$menu_item_id = $menu_item->ID;
+
+			// Current menu's parent id.
+			$menu_item_parent_id = absint( $menu_item->menu_item_parent );
+
+			// Is the current menu item a child of the parent item.
+			if ( $menu_item_parent_id === $parent_id ) {
+
+				// Get parsed menu item.
+				$clean_menu_item = menu_item()->parse_menu_post( $menu_item );
+
+				// Remove from loop.
+				unset( $menu_items[ $key ] );
+
+				// Recursively build children menu items.
+				$children_of_menu_item = $this->build_menu( $menu_items, $menu_item_id );
+
+				// If children menu items exist, set them as children.
+				if ( ! empty( $children_of_menu_item ) ) {
+					$clean_menu_item->set_children( $children_of_menu_item );
+				}
+
+				$menu[] = $clean_menu_item;
+			}
+		}
+
+		return (array) $menu;
 	}
 }
 
