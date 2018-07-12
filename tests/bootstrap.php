5b5ffe6b6fcd97eb2af87ade26c8130f7f6fd99f
Owen Stowe <ostowe@gmail.com>
2018-07-12 13:54:45 -0400

    Woops

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,7 +14,7 @@ if ( ! $_tests_dir ) {
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 	// PHPCS is complaining about this line not being translated, but it not really relevant for this file.
 	// phpcs:disable
-	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL;
 	exit( 1 );
 	// phpcs:enable
 }
