ebbd70d62b0bdcbec5b16cb35ed343c5a1a98ba8
James Burke <james.walter.burke@gmail.com>
2018-08-24 12:22:17 -0400

    Updating head component

--- inc/components/class-head.php
+++ inc/components/class-head.php
@@ -20,6 +20,22 @@ class Head extends Component {
 	public $name = 'head';
 
 	/**
+	 * Initalize this component.
+	 *
+	 * @param null|\WP_Query $wp_query WP_Query object or null.
+	 */
+	public function __construct( $wp_query = null ) {
+		parent::__construct();
+
+		// Set the default title.
+		$this->set_title( get_bloginfo( 'name' ) );
+
+		if ( $wp_query instanceof \WP_Query ) {
+			$this->parse_from_query( $wp_query );
+		}
+	}
+
+	/**
 	 * Define the default config of a head.
 	 *
 	 * @return array A default config.
@@ -29,10 +45,23 @@ class Head extends Component {
 	}
 
 	/**
+	 * Set the state of this component using a $wp_query object.
+	 *
+	 * @todo  Clean up this function and make it more verbose.
+	 *
+	 * @param \WP_Query $wp_query WP_Query object.
+	 */
+	public function parse_from_query( $wp_query ) {
+		if ( $wp_query->is_single() ) {
+			$this->set_title( $wp_query->post->post_title );
+		}
+	}
+
+	/**
 	 * Set a title tag as a child of the Head component.
 	 *
 	 * @param string $value The title value.
-	 * @return Head
+	 * @return Head The instance of this component.
 	 */
 	public function set_title( $value ) {
 		$title_component = ( new Component( 'title' ) )
@@ -50,17 +79,72 @@ class Head extends Component {
 	}
 
 	/**
-	 * Add a meta tag as a child of the Head component.
+	 * Helper function for setting a canonical url.
 	 *
-	 * @param string $property The meta property attribute.
-	 * @param string $content  The meta content attribute.
-	 * @return Head
+	 * @param  string $url Canonical URL.
+	 * @return Head The instance of this component.
+	 */
+	public function set_canonical_url( $url ) {
+		return $this->add_link( 'canonical', $url );
+	}
+
+	/**
+	 * Helper function for adding a new meta tag.
+	 *
+	 * @param string $property Property value.
+	 * @param string $content  Content value.
+	 * @return Head The instance of this component.
 	 */
 	public function add_meta( $property, $content ) {
-		return $this->set_children([
-			( new Component( 'meta' ) )
-				->set_config( 'property', $property )
-				->set_config( 'content', $content ),
-		], true );
+		return $this->add_tag(
+			'meta',
+			[
+				'property' => $property,
+				'content'  => $content,
+			]
+		);
+	}
+
+	/**
+	 * Helper function for adding a new link tag.
+	 *
+	 * @param string $rel  Rel value.
+	 * @param string $href Href value.
+	 * @return Head The instance of this component.
+	 */
+	public function add_link( $rel, $href ) {
+		return $this->add_tag(
+			'link',
+			[
+				'rel'  => $rel,
+				'href' => $href,
+			]
+		);
+	}
+
+	/**
+	 * Helper function to quickly add a new tag.
+	 *
+	 * @param  string $tag        Tag value.
+	 * @param  array  $attributes Tag attributes.
+	 * @return Head The instance of this component.
+	 */
+	protected function add_tag( $tag, $attributes = [] ) {
+
+		// Initalize a new component for this tag.
+		$component = new Component( $tag );
+
+		// Add all attributes.
+		foreach ( $attributes as $key => $value ) {
+			$component->set_config( $key, $value );
+		}
+
+		// Append this tag as a child component.
+		return $this->set_children(
+			[
+				$component,
+			],
+			true
+		);
 	}
 }
