2b2bd60624adb5e7bd72e8698c724c694d00b61c
Joe McGill <joemcgill@gmail.com>
2020-05-11 13:22:00 -0500

    Update PHPUnit tests
    - Add initial test class for templates
    - Clean up some PHPUnit bootstrapping behavior
    - Move the Test_Helpers class into the 'inc' dir
    - Ignore the 'vendor' folder

---
+++ tests/test-templates.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Class Test_Templates
+ *
+ * @package WP_Irving
+ */
+
+use WP_Irving\Templates;
+
+/**
+ * Test templates functionality.
+ *
+ * @group templates
+ */
+class Test_Templates extends WP_UnitTestCase {
+	function setup() {
+		parent::setUp();
+
+		// Hook up template path filters.
+		add_filter( 'wp_irving_template_path', function () {
+			return dirname( __FILE__ ) . '/inc/templates';
+		} );
+
+		add_filter( 'wp_irving_template_part_path', function () {
+			return dirname( __FILE__ ) . '/inc/template-parts';
+		} );
+	}
+
+	function get_template_paths() {
+		return [
+			[
+				[ 'defaults' ]
+			],
+			[
+				[ 'index' ]
+			],
+			[
+				[ 'single' ]
+			],
+		];
+	}
+
+	/**
+	 * Test that local template paths is working.
+	 *
+	 * @dataProvider get_template_paths
+	 *
+	 * @param array $paths
+	 */
+	function test_template_paths( $paths ) {
+		$path = Templates\locate_template( $paths );
+
+		$this->assertTrue( file_exists( $path ) );
+	}
+
+	/**
+	 * Test that local template partial paths is working.
+	 */
+	function test_template_partial_paths() {
+		$path = Templates\locate_template_part( 'sidebar' );
+
+		$this->assertTrue( file_exists( $path ) );
+	}
+}
