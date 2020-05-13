e041777c35dbad453077980369f1b4ccde0654bf
James Burke <james.walter.burke@gmail.com>
2020-05-13 17:24:53 -0500

    More testing

--- tests/test-component.php
+++ tests/test-component.php
@@ -13,31 +13,69 @@ use WP_Irving\Component;
 class Component_Tests extends WP_UnitTestCase {
 
 	/**
-	 * Test suite setup.
+	 * Associative array of components to use for testing.
+	 *
+	 * @var array
 	 */
-	public static function setUpBeforeClass() {
-	}
+	public static $components = [];
 
 	public function setUp() {
 		parent::setUp();
+		$this->create_test_components();
 	}
 
 	/**
-	 * Test the component constructor.
+	 * Loop through the directory with example component data and initialize
+	 * new class instances.
 	 */
-	public function test_component_constructor() {
+	public function create_test_components() {
+
+		// Loop through each file in the /component-data/ directory.
+		foreach ( scandir( __DIR__ . '/component-data/' ) as $file_name ) {
+
+			// Validate the path.
+			$path = sprintf( __DIR__ . '/component-data/%1$s', $file_name );
+			if ( ! file_exists( $path ) ) {
+				continue;
+			}
+
+			$component = file_get_contents( $path ); // phpcs:ignore
+			$component = json_decode( $component, TRUE );
+
+			if ( is_null( $component ) ) {
+				assert( false, 'Could not load ' . $file_name );
+				continue;
+			}
+
+			self::$components[ str_replace( '.json', '', $file_name ) ] = new Component( $component['name'], $component );
+		}
 	}
 
+	/**
+	 * Get a test component by the name.
+	 *
+	 * @param string $name Name
+	 * @return ?Component
+	 */
+	public function get_component( $name ) {
+		if ( isset( self::$components[ $name ] ) ) {
+			return self::$components[ $name ];
+		}
+
+		return assert( false, 'Could not load ' . $name );
+	}
+
+	/**
+	 * Test the name functionality.
+	 */
 	public function test_name() {
 
-		// Test name being set.
-		$no_namespace      = new Component( 'example' );
-		$correct_namespace = new Component( 'irving/example' );
-		$too_many_slashes  = new Component( 'irving/layouts/example' );
+		// Name.
+		$this->assertEquals( 'example', $this->get_component( 'example-001' )->get_name() );
+		$this->assertEquals( 'irving/example', $this->get_component( 'irving-namespace' )->get_name() );
 
-		// Test namespaces.
-		$this->assertEmpty( '', $no_namespace->get_namespace(), 'Expected an empty string.' );
-		$this->assertEquals( 'irving', $correct_namespace->get_namespace() );
-		$this->assertEquals( 'irving', $too_many_slashes->get_namespace() );
+		// Namespaces.
+		$this->assertEquals( '', $this->get_component( 'example-001' )->get_namespace(), 'Expected an empty string.' );
+		$this->assertEquals( 'irving', $this->get_component( 'irving-namespace' )->get_namespace() );
 	}
 }
