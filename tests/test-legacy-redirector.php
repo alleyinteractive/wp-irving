677ec3c66b8e15ad74ae449d74d1a6c0ee419c34
James Burke <james.walter.burke@gmail.com>
2020-05-14 14:41:57 -0500

    Update redirect unit tests

--- tests/test-legacy-redirector.php
+++ tests/test-legacy-redirector.php
@@ -30,23 +30,29 @@ class Legacy_Redirector_Tests extends WP_UnitTestCase {
 	 */
 	public static function setUpBeforeClass() {
 		self::$helpers = new \WP_Irving\Test_Helpers();
-		add_filter( 'wpcom_legacy_redirector_allow_insert', '__return_true', 10, 1 );
 	}
 
 	/**
 	 * Skip tests if necessary.
 	 */
 	public function setUp() {
+		parent::setUp();
+
 		if ( ! defined( 'WPCOM_LEGACY_REDIRECTOR_VERSION' ) ) {
 			$this->markTestSkipped( 'WPCOM Legacy Redirector is not installed.' );
 		}
+
+		// Ensure that we have permission to insert redirects.
+		add_filter( 'wpcom_legacy_redirector_allow_insert', '__return_true', 10, 1 );
 	}
 
 	/**
 	 * Test relative redirect.
 	 */
 	public function test_relative_to_relative() {
-		WPCOM_Legacy_Redirector::insert_legacy_redirect( '/foo/', '/bar/', false );
+		$created_redirect = WPCOM_Legacy_Redirector::insert_legacy_redirect( '/foo/', '/bar/', false );
+		$this->assertEquals( true, $created_redirect, 'Could not insert redirect for /foo/bar/' );
+
 		$response = self::$helpers->get_components_endpoint_response( '/foo/' );
 		$this->assertEquals( 'http://example.org/bar/', $response->data['redirectTo'] );
 	}
@@ -56,12 +62,16 @@ class Legacy_Redirector_Tests extends WP_UnitTestCase {
 	 */
 	public function test_relative_to_absolute() {
 		// Internal, absolute URL destination.
-		WPCOM_Legacy_Redirector::insert_legacy_redirect( '/foo/bar/', 'http://example.org/baz/', false );
+		$created_redirect = WPCOM_Legacy_Redirector::insert_legacy_redirect( '/foo/bar/', 'http://example.org/baz/', false );
+		$this->assertEquals( true, $created_redirect, 'Could not insert redirect for /foo/bar/' );
+
 		$response = self::$helpers->get_components_endpoint_response( '/foo/bar/' );
 		$this->assertEquals( 'http://example.org/baz/', $response->data['redirectTo'] );
 
 		// External, absolute URL destination.
-		WPCOM_Legacy_Redirector::insert_legacy_redirect( '/foo/bar/baz/', 'http://another-example.org/baz/', false );
+		$created_redirect = WPCOM_Legacy_Redirector::insert_legacy_redirect( '/foo/bar/baz/', 'http://another-example.org/baz/', false );
+		$this->assertEquals( true, $created_redirect, 'Could not insert redirect for /foo/bar/' );
+
 		$response = self::$helpers->get_components_endpoint_response( '/foo/bar/baz/' );
 		$this->assertEquals( 'http://another-example.org/baz/', $response->data['redirectTo'] );
 	}
@@ -70,7 +80,9 @@ class Legacy_Redirector_Tests extends WP_UnitTestCase {
 	 * Test redirects from an absolute, internal URL to a relative URL
 	 */
 	public function test_absolute_to_relative() {
-		WPCOM_Legacy_Redirector::insert_legacy_redirect( 'http://example.org/foo/', '/bar/', false );
+		$created_redirect = WPCOM_Legacy_Redirector::insert_legacy_redirect( 'http://example.org/foo/', '/bar/', false );
+		$this->assertEquals( true, $created_redirect, 'Could not insert redirect for /foo/bar/' );
+
 		$response = self::$helpers->get_components_endpoint_response( '/foo/' );
 		$this->assertEquals( 'http://example.org/bar/', $response->data['redirectTo'] );
 	}
@@ -80,12 +92,16 @@ class Legacy_Redirector_Tests extends WP_UnitTestCase {
 	 */
 	public function test_absolute_to_absolute() {
 		// Internal, absolute URL destination.
-		WPCOM_Legacy_Redirector::insert_legacy_redirect( 'http://example.org/foo/bar/', 'http://example.org/baz/', false );
+		$created_redirect = WPCOM_Legacy_Redirector::insert_legacy_redirect( 'http://example.org/foo/bar/', 'http://example.org/baz/', false );
+		$this->assertEquals( true, $created_redirect, 'Could not insert redirect for /foo/bar/' );
+
 		$response = self::$helpers->get_components_endpoint_response( '/foo/bar/' );
 		$this->assertEquals( 'http://example.org/baz/', $response->data['redirectTo'] );
 
 		// External URL destination.
-		WPCOM_Legacy_Redirector::insert_legacy_redirect( 'http://example.org/foo/bar/baz/', 'http://another-example.org/baz/', false );
+		$created_redirect = WPCOM_Legacy_Redirector::insert_legacy_redirect( 'http://example.org/foo/bar/baz/', 'http://another-example.org/baz/', false );
+		$this->assertEquals( true, $created_redirect, 'Could not insert redirect for /foo/bar/' );
+
 		$response = self::$helpers->get_components_endpoint_response( '/foo/bar/baz/' );
 		$this->assertEquals( 'http://another-example.org/baz/', $response->data['redirectTo'] );
 	}
@@ -95,7 +111,8 @@ class Legacy_Redirector_Tests extends WP_UnitTestCase {
 	 */
 	public function test_with_trailing_slashes() {
 		// Redirect from with trailing slash.
-		WPCOM_Legacy_Redirector::insert_legacy_redirect( '/trailing-slash/', '/destination/', false );
+		$created_redirect = WPCOM_Legacy_Redirector::insert_legacy_redirect( '/trailing-slash/', '/destination/', false );
+		$this->assertEquals( true, $created_redirect, 'Could not insert redirect for /foo/bar/' );
 
 		// With trailing slash.
 		$response = self::$helpers->get_components_endpoint_response( '/trailing-slash/' );
