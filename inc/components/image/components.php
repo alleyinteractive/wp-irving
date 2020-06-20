fd53d37b434700cdddb2828bb20639f941a6032c
James Burke <james.walter.burke@gmail.com>
2020-06-20 13:41:42 -0400

    Temp commit

---
+++ inc/components/image/components.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Image
+ *
+ * Image.
+ *
+ * @package WP_Irving
+ */
+
+namespace WP_Irving\Components;
+
+use WP_Irving\Component;
+
+if ( ! function_exists( '\WP_Irving\get_registry' ) ) {
+	return;
+}
+
+/**
+ * Register the component and callback.
+ */
+\WP_Irving\get_registry()->register_component_from_config( __DIR__ . '/component' );
--- inc/components/post-featured-media/component.php
+++ inc/components/post-featured-media/component.php
@@ -29,41 +29,13 @@ if ( ! function_exists( '\WP_Irving\get_registry' ) ) {
 			// Get the post ID from a context provider.
 			$post_id = $component->get_config( 'post_id' );
 
-			// Get and validate image url.
-			$image_url = get_the_post_thumbnail_url( $post_id );
-			if ( empty( $image_url ) ) {
-				return $component;
-			}
-
-			$wrapper = new Component( 'irving/container' );
-			$wrapper->set_config( 'max_width', 'lg' );
-			$wrapper->set_config( 'style', [ 'bottom-margin' => '1rem' ] );
-
-			$wrapper->append_child(
-				( new Component( 'material/card-content' ) )
-					->set_config( 'gutter_bottom', true )
-					->set_child(
-						( new Component( 'material/card-media' ) )
-							->set_config( 'image', $image_url )
-							->set_config(
-								'style',
-								[
-									'bottom_margin' => '1rem',
-									'height'        => '450px',
-								]
-							)
-					)
+			$component->append_child(
+				[
+					'name' => 'irving/post-featured-image',
+				]
 			);
 
-			$caption = wp_get_attachment_caption( get_post_thumbnail_id( $post_id ) );
-			if ( ! empty( $caption ) ) {
-				$wrapper->append_child(
-					( new Component( 'irving/html' ) )
-						->append_child( $caption )
-				);
-			}
-
-			return $component->set_child( $wrapper );
+			return $component;
 		},
 	]
 );
