3bfb37a163fa8bdea7e331717948f46749e293d5
Tyler Reckart <tyler@alley.co>
2020-07-21 10:25:59 -0400

    IRV-384: register cache endpoint

---
+++ inc/endpoints/class-cache-endpoint.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Class file for the cache endpoint.
+ *
+ * @package WP_Irving
+ */
+namespace WP_Irving\REST_API;
+
+/**
+ * Cache Endpoint.
+ */
+class Cache_Endpoint extends Endpoint {
+    /**
+     * Attach to required hooks for endpoint.
+     */
+    public function __construct() {
+        parent::__construct();
+
+        add_action( 'rest_api_init', [ $this, 'register_rest_routes' ] );
+    }
+
+    /**
+     * Register the REST API routes.
+     */
+    public function register_rest_routes() {
+        register_rest_route(
+            self::get_namespace(),
+            '/purge-cache',
+            [
+                // 'methods'  => \WP_REST_Server::CREATABLE,
+                'methods'  => \WP_REST_Server::READABLE,
+                'callback' => [ $this, 'get_route_response' ],
+            ]
+        );
+    }
+
+	/**
+	 * Callback for the route.
+	 *
+	 * @param  WP_REST_Request $request Request object.
+	 *
+	 * @return array
+	 */
+    public function get_route_response( $request ) {
+        // phpcs:ignore WordPress.WP.GlobalVariablesOverride.OverrideProhibited
+        global $wp;
+    }
+}
