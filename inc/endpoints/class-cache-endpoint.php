3b6586da560f24f4fe13ef936b7f8bd659034779
Tyler Reckart <tyler@alley.co>
2020-07-21 14:28:33 -0400

    IRV-384: move action logic into endpoint

--- inc/endpoints/class-cache-endpoint.php
+++ inc/endpoints/class-cache-endpoint.php
@@ -16,7 +16,11 @@ class Cache_Endpoint extends Endpoint {
     public function __construct() {
         parent::__construct();
 
+        // Register the route.
         add_action( 'rest_api_init', [ $this, 'register_rest_routes' ] );
+        // Cache purge actions.
+        add_action( 'wp_irving_site_cache_purge', [ $this, 'purge_site_cache' ] );
+        add_action( 'wp_irving_page_cache_purge', [ $this, 'purge_page_cache' ] );
     }
 
     /**
@@ -37,10 +41,48 @@ class Cache_Endpoint extends Endpoint {
 	 * Callback for the route.
 	 *
 	 * @param  WP_REST_Request $request Request object.
-	 *
-	 * @return array
 	 */
     public function get_route_response( $request ) {
-        return $request;
+        // Get the request body.
+        $request_params = $request->get_body_params();
+        // Get the requested action.
+        $action = $request_params['action'];
+
+        // Ensure the action exists prior to firing.
+        if ( !empty( $action ) ) {
+            if ( $action === 'irving_site_cache_purge' ) {
+                do_action( 'wp_' . $action );
+            }
+
+            if ( $action === 'irving_page_cache_purge' ) {
+                // Get the route.
+                $route = request_params['route'];
+                // Ensure the requested route is not empty prior
+                // to firing the action.
+                if ( !empty( $route ) ) {
+                    do_action( 'wp_' . $action, $route );
+                }
+            }
+        }
+    }
+
+    /**
+     * Purge the site cache.
+     */
+    public function purge_site_cache() {
+        if ( function_exists( 'pantheon_wp_clear_edge_paths' ) ) {
+            \WP_Irving\Pantheon::pantheon_flush_site();
+        } else {
+            \WP_Irving\Cache::instance()->fire_wipe_request();
+        }
+    }
+
+    /**
+     * Purge the cache for a specific route/page.
+     *
+     * @param string $route The target route to purge from the cache.
+     */
+    public function purge_page_cache( $args ) {
+        // Do something.
     }
 }
