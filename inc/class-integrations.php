602ac6c9766d9310e32efe93c46fe6aa0a453ac2
Tyler Reckart <tyler@alley.co>
2020-07-30 13:05:13 -0400

    save the option

--- inc/class-integrations.php
+++ inc/class-integrations.php
@@ -13,6 +13,12 @@
 class Integrations {
 
     /**
+     * Holds the option values to be set.
+     */
+    private $options;
+
+
+    /**
      * Class instance.
      *
      * @var null|self
@@ -47,14 +53,14 @@ class Integrations {
      */
     public function register_settings_fields() {
         // Register a new setting for the integrations manager to consume/set.
-        register_setting( 'wp-irving-integrations', 'irving_integrations' );
+        register_setting( 'wp_irving_integrations', 'irving_integrations' );
 
         // Register the section.
         add_settings_section(
             'irving_integrations_settings',
             __( 'Add keys for integrations to be passed to the front-end.', 'wp-irving' ),
-            [ $this, 'set_irving_integrations_keys' ],
-            'irving_integrations'
+            '',
+            'wp_irving_integrations'
         );
 
         // Register a new field for the Google Analytics integration.
@@ -62,7 +68,7 @@ class Integrations {
             'irving_integrations_ga_key',
             __( 'Google Analytics Tracking ID', 'wp-irving' ),
             [ $this, 'get_ga_key' ],
-            'irving_integrations',
+            'wp_irving_integrations',
             'irving_integrations_settings',
             [
                 'id' => 'irving_integrations_ga_key',
@@ -70,16 +76,11 @@ class Integrations {
         );
     }
 
-    public function set_irving_integrations_keys() {
-        ?>
-            <p><?php esc_html_e( 'Follow the white rabbit.', 'wp-irving' ); ?></p>
-        <?php
-    }
-
     public function get_ga_key( $args ) {
-        $options = get_option( 'irving_integrations_options' );
+        $ga_key = $this->options[ $args[ 'id' ] ];
+
         ?>
-            <input type="text" name="<?php echo esc_attr( $args[ 'id' ] ); ?>" value="<?php echo esc_attr( $options[ $args[ 'id' ] ] ); ?>"/>
+            <input type="text" name="irving_integrations[<?php echo esc_attr( $args[ 'id' ] ); ?>]" value="<?php echo isset( $ga_key ) ? esc_attr( $ga_key ) : '' ?>" />
         <?php
     }
 
@@ -101,13 +102,17 @@ class Integrations {
      * Render the settings page.
      */
     public function render() {
+        $this->options = get_option( 'irving_integrations' );
+
         // Check if the user have submitted the settings
         if ( isset( $_GET['settings-updated'] ) ) {
-            add_settings_error( 'irving_integrations_messages', 'irving_integrations_message', __( 'Settings Saved', 'wporg' ) );
+            add_settings_error(
+                'irving_integrations_messages',
+                'irving_integrations_message',
+                __( 'Settings Saved', 'wp-irving' ),
+                'updated'
+            );
         }
-
-         // Show error/update messages
-        settings_errors( 'irving_integrations_messages' );
 
         ?>
             <div class="wrap">
@@ -118,10 +123,8 @@ class Integrations {
                 <hr class="wp-header-end">
 
                 <form method="post" action="options.php">
-                    <?php settings_fields( 'irving_integrations' ); ?>
-
-                    <?php do_settings_sections( 'irving_integrations' ); ?>
-
+                    <?php settings_fields( 'wp_irving_integrations' ); ?>
+                    <?php do_settings_sections( 'wp_irving_integrations' ); ?>
                     <?php submit_button( __( 'Save Settings', 'wp-irving' ) ); ?>
                 </form>
             </div>
