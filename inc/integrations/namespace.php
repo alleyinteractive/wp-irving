22c970b99a2fce0f8b0640a491c8b29094303b39
Tyler Reckart <tyler@alley.co>
2020-08-09 10:51:12 -0400

    IRV-592: add doc comments & format

--- inc/integrations/namespace.php
+++ inc/integrations/namespace.php
@@ -11,59 +11,67 @@ namespace WP_Irving\Integrations;
  * Hook namespace functionality.
  */
 function bootstrap() {
-    add_filter( 'init', __NAMESPACE__ . '\load_integrations_manager' );
-    add_filter( 'plugins_loaded', __NAMESPACE__ . '\auto_register_plugin_integrations' );
+	add_filter( 'init', __NAMESPACE__ . '\load_integrations_manager' );
+	add_filter( 'plugins_loaded', __NAMESPACE__ . '\auto_register_plugin_integrations' );
 }
 
+/**
+ * Load the integrations manager and register the base integrations.
+ */
 function load_integrations_manager() {
-    // Instantiate the integrations manager.
-    $manager = new \WP_Irving\Integrations\Integrations_Manager();
-    $manager->setup();
+	// Instantiate the integrations manager.
+	$manager = new \WP_Irving\Integrations\Integrations_Manager();
+	$manager->setup();
 
-    // Register integrations.
-    auto_register_integrations();
+	// Register integrations.
+	auto_register_integrations();
 }
 
 /**
  * Auto load the integrations.
  */
 function auto_register_integrations() {
-    $integrations = [
-        'archiveless'             => __NAMESPACE__ . '\Archiveless',
-        'google_analytics'        => __NAMESPACE__ . '\Google_Analytics',
-        'google_amp'              => __NAMESPACE__ . '\Google_AMP',
-        'new_relic'               => __NAMESPACE__ . '\New_Relic',
-        'pantheon'                => __NAMESPACE__ . '\Pantheon',
-        'safe_redirect_manager'   => __NAMESPACE__ . '\Safe_Redirect_Manager',
-        'vip_go'                  => __NAMESPACE__ . '\VIP_Go',
-        'wpcom_legacy_redirector' => __NAMESPACE__ . '\WPCOM_Legacy_Redirector',
-        'yoast'                   => __NAMESPACE__ . '\Yoast',
-    ];
+	$integrations = [
+		'archiveless'             => __NAMESPACE__ . '\Archiveless',
+		'google_analytics'        => __NAMESPACE__ . '\Google_Analytics',
+		'google_amp'              => __NAMESPACE__ . '\Google_AMP',
+		'new_relic'               => __NAMESPACE__ . '\New_Relic',
+		'pantheon'                => __NAMESPACE__ . '\Pantheon',
+		'safe_redirect_manager'   => __NAMESPACE__ . '\Safe_Redirect_Manager',
+		'vip_go'                  => __NAMESPACE__ . '\VIP_Go',
+		'wpcom_legacy_redirector' => __NAMESPACE__ . '\WPCOM_Legacy_Redirector',
+		'yoast'                   => __NAMESPACE__ . '\Yoast',
+	];
 
-    
-    // Allow new integrations to be added via a filter.
-    $integrations = apply_filters( 'wp_irving_active_integrations', $integrations );
-    
-    load_integrations( $integrations );
+	// Allow new integrations to be added via a filter.
+	$integrations = apply_filters( 'wp_irving_active_integrations', $integrations );
+
+	load_integrations( $integrations );
 }
 
 /**
  * Auto load the plugin-based integrations.
  */
 function auto_register_plugin_integrations() {
-    $integrations = [
-        'jwt_auth' => __NAMESPACE__ . '\JWT_Auth',
-    ];
+	$integrations = [
+		'jwt_auth' => __NAMESPACE__ . '\JWT_Auth',
+	];
 
-    // Allow new integrations to be added via a filter.
-    $integrations = apply_filters( 'wp_irving_active_plugin_integrations', $integrations );
+	// Allow new integrations to be added via a filter.
+	$integrations = apply_filters( 'wp_irving_active_plugin_integrations', $integrations );
 
-    load_integrations( $integrations );
+	load_integrations( $integrations );
 }
 
+/**
+ * Load the integrations provided by the autoload functions
+ * or via the active integrations filters.
+ *
+ * @param array $integrations The integrations to instantiate.
+ */
 function load_integrations( array $integrations ) {
-    foreach ( $integrations as $type => $class ) {
-        $integration = new $class();
-        $integration->setup();
-    }
+	foreach ( $integrations as $type => $class ) {
+		$integration = new $class();
+		$integration->setup();
+	}
 }
