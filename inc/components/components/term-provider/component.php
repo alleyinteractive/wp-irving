df6856c3688010d3f3f0114ddb081ae1ebb9ebba
James Burke <james.walter.burke@gmail.com>
2020-08-15 19:36:47 -0400

    Term provider, name, and link components

---
+++ inc/components/components/term-provider/component.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Term provider.
+ *
+ * Provide term context to children components.
+ *
+ * @package WP_Irving
+ */
+
+namespace WP_Irving\Components;
+
+/**
+ * Register the component.
+ */
+register_component_from_config( __DIR__ . '/component' );
