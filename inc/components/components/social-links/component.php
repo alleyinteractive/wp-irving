5d1fe001b6fd7f1d2d0f3ee1714026e45e7dc279
Matt Johnson <matt@alley.co>
2020-08-24 08:27:48 -0700

    fix phpcs

--- inc/components/components/social-links/component.php
+++ inc/components/components/social-links/component.php
@@ -17,11 +17,12 @@ register_component_from_config(
 	[
 		'config_callback' => function ( array $config ): array {
 			$config['platforms'] = $config['platforms'] ?? [];
+			
 			$platforms = apply_filters( 'wp_irving_social_links_platforms', $config['platforms'] );
 
 			$config['platforms'] = is_array( $platforms ) ? $platforms : [];
 
 			return $config;
-		}
+		},
 	]
 );
