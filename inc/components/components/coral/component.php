b1c25e4d860367e62e940f6d1fd5602e989580f5
Joe McGill <joemcgill@gmail.com>
2020-08-26 10:57:14 -0500

    Integrations: Automatically hydrate irving/coral embed_url
    This adds a config_callback to the `irving/coral` component that gets
    the option value for the embed_url from the Integrations settings.
    
    Adds unit test for the component.

--- inc/components/components/coral/component.php
+++ inc/components/components/coral/component.php
@@ -9,7 +9,21 @@
 
 namespace WP_Irving\Components;
 
+use WP_Irving\Integrations;
+
 /**
  * Register the component.
  */
-register_component_from_config( __DIR__ . '/component' );
+register_component_from_config(
+	__DIR__ . '/component',
+	[
+		'config_callback' => function ( $config ) {
+			return array_merge(
+				$config,
+				[
+					'embed_URL' => untrailingslashit( Integrations\get_option_value( 'coral', 'url' ) ),
+				]
+			);
+		},
+	]
+);
