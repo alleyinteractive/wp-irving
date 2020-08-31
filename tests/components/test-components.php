2f0f2058f2db03a61b9a24343abd223e9acafe72
James Burke <james.walter.burke@gmail.com>
2020-08-31 17:38:37 -0400

    Update unit tests

--- tests/components/test-components.php
+++ tests/components/test-components.php
@@ -367,6 +367,8 @@ class Test_Components extends WP_UnitTestCase {
 				'html'         => true,
 				'oembed'       => true,
 				'postId'       => $this->get_post_id(),
+				'style'        => [],
+				'tag'          => 'span',
 				'themeName'    => 'default',
 				'themeOptions' => [ 'default' ],
 			],
