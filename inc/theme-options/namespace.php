8a1bbfc218b125b60e1c6b2550583a3108e86217
James Burke <james.walter.burke@gmail.com>
2020-09-28 17:55:51 -0400

    Remove unused function

--- inc/theme-options/namespace.php
+++ inc/theme-options/namespace.php
@@ -6,25 +6,3 @@
  */
 
 namespace WP_Irving\Theme_Options;
-
-/**
- * Hook namespace functionality.
- */
-function bootstrap() {
-	add_filter( 'init', __NAMESPACE__ . '\auto_register_components' );
-}
-
-function add_theme_option( array $args ) {
-	$args = wp_parse_args(
-		$args,
-		[
-			'callback' => '',
-			'default'  => '',
-			'key'      => '',
-			'label'    => '',
-			'location' => '',
-			'option'   => '',
-			'type'     => '',
-		]
-	);
-}
