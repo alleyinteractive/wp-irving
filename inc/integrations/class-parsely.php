b54b233cc7c05b30341032758d4ca14d27ef5676
Davis Shaver <davisshaver@gmail.com>
2020-11-26 16:58:22 -0500

    LEDE-435: Use Parse.ly JSON-LD in Irving

--- inc/integrations/class-parsely.php
+++ inc/integrations/class-parsely.php
@@ -7,6 +7,7 @@
 
 namespace WP_Irving\Integrations;
 
+use WP_Irving\Components;
 use WP_Irving\Singleton;
 
 /**
@@ -19,9 +20,48 @@ class Parsely {
 	 * Setup the integration.
 	 */
 	public function setup() {
-		// Ensure Pico exists and is enabled.
-		if ( ! defined( 'PICO_VERSION' ) ) {
+		// Ensure Parse.ly plugins exists and is enabled.
+		if ( ! defined( 'PARSELY_VERSION' ) ) {
 			return;
 		}
+
+		if ( ! is_admin() ) {
+			// Parse Parse.ly's head markup and inject it into the Head component.
+			add_filter( 'wp_irving_component_children', [ $this, 'inject_parsely_tags_into_head_children' ], 10, 3 );
+		}
+	}
+
+	/**
+	 * Parse Parse.ly's head markup, inject into the Head component.
+	 *
+	 * @param array  $children Children for this component.
+	 * @param array  $config   Config for this component.
+	 * @param string $name     Name of this component.
+	 * @return array
+	 */
+	public function inject_parsely_tags_into_head_children( array $children, array $config, string $name ): array {
+		// Ony run this action on the `irving/head` in a `page` context.
+		if (
+			'irving/head' !== $name
+			|| 'page' !== ( $config['context'] ?? 'page' )
+		) {
+			return $children;
+		}
+		return array_merge(
+			$children,
+			Components\html_to_components( $this->get_parsely_site_markup(), [ 'meta', 'script' ] )
+		);
+	}
+
+	/**
+	 * Capture the markup output by Parse.ly in the site <head>.
+	 *
+	 * @return string
+	 */
+	public function get_parsely_site_markup(): string {
+		global $parsely;
+		ob_start();
+		$parsely->insert_parsely_page();
+		return ob_get_clean();
 	}
 }
